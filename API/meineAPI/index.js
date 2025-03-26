2418aafe7f1a4ebadf05dcd3db227ebb2fc8d177
Stitch <dustin.poetzsch@tn.techstarter.de>
2025-03-26 17:38:08 +0100

    API Anfragen 26.03.25

---
+++ API/meineAPI/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+let giveMeAJoke = require('give-me-a-joke');
+
+let generateName = require('sillyname');
+
+
+
+app.get("/", (req, res) => {
+    res.send("Willkommen bei meiner eigenen API!");
+});
+
+app.get("/data", (req, res) => {
+    res.json([
+        { id: 1, name: "Max" },
+        { id: 2, name: "Lena" }
+    ]);
+});
+
+
+app.get("/randomname", (req, res) => {
+    let sillyName = generateName();
+    res.send(sillyName)
+
+});
+
+app.get("/joke", (req, res) => {
+    giveMeAJoke.getRandomCNJoke(function (joke) {
+        res.send(joke);
+    });
+});
+
+
+
+app.listen(3001, () => {
+    console.log("Server läuft auf http://localhost:3001&quot;");
+});
